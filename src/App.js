2894ea1d1053c2f9b3b60dfb60e8fe85eaef2fb1
jacobtdp <jacobsantiagodennis@gmail.com>
2020-04-23 16:28:45 -0700

    Navbar layout w/ GRID

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Nav from './nav/Nav';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Nav />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/nav/Nav.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Nav.css';
+
+class Nav extends Component{
+
+    render(){
+        return(
+            <div className="navbar">
+                <h2 className="nested">Title</h2>
+                <p className="nested">Apple</p>
+                <p className="nested">Orange</p>
+                <p className="nested">Cherry</p>
+                <p className="nested">Coconut</p>
+
+                <div></div>
+                <div><h2>Q</h2></div>
+            </div>
+        )
+    }
+
+}
+
+export default Nav;
