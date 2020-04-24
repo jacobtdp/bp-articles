348d05575c4c195082d342f2afc322e2718f0d58
jacobtdp <jacobsantiagodennis@gmail.com>
2020-04-23 18:00:53 -0700

    main content layout w/ GRID

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import './App.css';
+
+import Main from './main/Main';
 
 import Nav from './nav/Nav';
 
@@ -6,8 +9,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        
         <Nav />
+
       </header>
+
+        <div className="body-wrapper">
+          <div>.</div>
+          <div><Main /></div>
+          <div>.</div>
+        </div>
+
     </div>
   );
 }
---
+++ src/main/Main.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+class Main extends Component{
+
+    render(){
+        return(
+            <div className="main-content">
+                <div className="title-story-link one">1</div>
+                <div className="title-story-link">3</div>
+                <div className="title-story-link">2</div>
+
+                <div className="tbd-content">
+                    <div className="title-story-link">4</div>
+                    <div className="title-story-link">4</div>
+                    <div className="title-story-link">4</div>
+                    <div className="title-story-link">4</div>
+                    <div className="title-story-link">4</div>
+                    <div className="title-story-link">4</div>
+                    <div className="title-story-link">4</div>
+                    <div className="title-story-link">4</div>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Main;
